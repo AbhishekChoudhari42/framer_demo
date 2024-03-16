3c86422fd07720debfe5c27dfe008ddb64958fdb
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-03-16 20:31:15 +0530

    horizontal scroll added

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between">
       <Overlay />
-      {/* <HorizontalScroll /> */}
+      <HorizontalScroll />
       <ThreeDScroll />
     </div>
   );
